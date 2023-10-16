f019c503f04526107a990eb2e11baa1af41c718b
sivajithms <sivajithm@gmail.com>
2023-10-16 22:17:51 +0530

    code rearrange

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,5 @@
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
 */
-
 // This file is intentionally blank
-
 // Use this file to add JavaScript to your project
